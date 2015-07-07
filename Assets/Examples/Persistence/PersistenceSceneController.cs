abad03a8f8976240877275d9ef874344249fd148
Christian Engvall <crilleengvall@gmail.com>
2015-07-07 22:30:12 +0200

    Step 5 - Adding PersistenceSceneController

---
+++ Assets/Examples/Persistence/PersistenceSceneController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PersistenceSceneController : MonoBehaviour {
+	public Text Text;
+	public InputField InputField;
+
+	public void SaveInputFieldValue() {
+	}
+}
