59dceacb870878b989da14cdb96a6a829ad6c4ef
Christian Engvall <crilleengvall@gmail.com>
2015-07-07 23:05:46 +0200

    Step 7 - saving highscore

--- Assets/Examples/Persistence/PersistenceSceneController.cs
+++ Assets/Examples/Persistence/PersistenceSceneController.cs
@@ -7,5 +7,26 @@ public class PersistenceSceneController : MonoBehaviour {
 	public InputField InputField;
 
 	public void SaveInputFieldValue() {
+		var inputFieldValue = 0;
+		if(int.TryParse(this.InputField.text, out inputFieldValue)) {
+			this.SaveToDisc();
+		}
+	}
+
+	private void SaveToDisc() {
+		var highScore = this.GetHighScoreFromField();
+		PersistenceManager.Instance.Save(highScore);
+	}
+
+	private HighScore GetHighScoreFromField() {
+		var scoreFromInput = int.Parse(this.InputField.text);
+		var highScore = new HighScore();
+		highScore.score = scoreFromInput;
+		return highScore;
+	}
+
+	void Start() {
+		var score = PersistenceManager.Instance.Load(HighScore.NameOfFile) as HighScore;
+		Text.text = score.score.ToString();
 	}
 }
