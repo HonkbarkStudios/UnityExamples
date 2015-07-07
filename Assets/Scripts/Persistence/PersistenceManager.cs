ce8bf9bfa21a749d22a2426e78dda89a9b2e39c0
Christian Engvall <crilleengvall@gmail.com>
2015-07-07 21:57:06 +0200

    Step 3 and 4 - Adding code to persistence manager

--- Assets/Scripts/Persistence/PersistenceManager.cs
+++ Assets/Scripts/Persistence/PersistenceManager.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class PersistenceManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	public static PersistenceManager Instance;
+	
+	void Awake() {
+		Environment.SetEnvironmentVariable("MONO_REFLECTION_SERIALIZER","yes");
+		Instance = this;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	public void Save(IPersistence objectToSave) {
+		BinaryFormatter formatter = new BinaryFormatter();
+		FileStream file = File.Open(Application.persistentDataPath + "/" + objectToSave.FileName, FileMode.OpenOrCreate);
+		formatter.Serialize(file, objectToSave);
+		file.Close();
+	}
 	
+	public System.Object Load(string nameOfFile) {
+		var serializedObject = new System.Object();
+		if(File.Exists(Application.persistentDataPath + "/" + nameOfFile)) {
+			BinaryFormatter formatter = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/" + nameOfFile, FileMode.Open);
+			serializedObject = formatter.Deserialize(file);
+			file.Close();
+		}
+		return serializedObject;
 	}
 }
