c57c388bd47e58773e9965f29dbf356a1106e27f
Christian Engvall <crilleengvall@gmail.com>
2015-07-07 22:48:58 +0200

    Step 6 - Adding a serializable class to save

---
+++ Assets/Examples/Persistence/HighScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[Serializable]
+public class HighScore : IPersistence {
+	public int score;
+	public const string NameOfFile = "highscore.dat";
+	public string FileName {
+		get {
+			return NameOfFile;
+		}
+	}
+}
