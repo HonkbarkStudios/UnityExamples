a23762949bac7b94f5aab0e8935eab3dcb2637a4
Christian Engvall <crilleengvall@gmail.com>
2015-08-04 14:12:23 +0200

    Step 4 Volume - Adding ToggleMusic method

--- Assets/AudioHandlerController.cs
+++ Assets/AudioHandlerController.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class AudioHandlerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void ToggleMusic() {
+		if(AudioListener.volume == 0.0f) {
+			AudioListener.volume = 1.0f;
+		}
+		else if(AudioListener.volume == 1.0f) {
+			AudioListener.volume = 0.0f;
+		}
 	}
 }
