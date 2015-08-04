7aedf9d944eba063b895d9b41318624ae4fe8604
Christian Engvall <crilleengvall@gmail.com>
2015-08-04 14:32:50 +0200

    Step 6 Volume - Changing button text

--- Assets/AudioHandlerController.cs
+++ Assets/AudioHandlerController.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class AudioHandlerController : MonoBehaviour {
+
+	public Text ButtonText;
 
 	public void ToggleMusic() {
 		if(AudioListener.volume == 0.0f) {
 			AudioListener.volume = 1.0f;
+			this.UpdateButtonText("off");
 		}
 		else if(AudioListener.volume == 1.0f) {
 			AudioListener.volume = 0.0f;
+			this.UpdateButtonText("on");
 		}
+	}
+
+	private void UpdateButtonText(string value) {
+		ButtonText.text = "Turn music " + value;
 	}
 }
