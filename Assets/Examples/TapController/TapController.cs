0c6c2aa5e91df80145dcacae1af286bab68ae525
Christian Engvall <crilleengvall@gmail.com>
2016-06-07 11:23:13 +0200

    Step 2 TapController - Adding script

---
+++ Assets/Examples/TapController/TapController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapController : MonoBehaviour {
+
+	public Rigidbody rigidBody;
+	public Vector2 jumpForce = new Vector2(0, 4f);
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+
+	}
+}
